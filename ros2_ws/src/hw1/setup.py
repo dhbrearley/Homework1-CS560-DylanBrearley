5f9bc2841267bbc883ef255006164107ce397bf4
Dylan Brearley <dhbrearley@crimson.ua.edu>
2025-09-16 23:04:49 -0400

    fixed setup, config files

---
+++ ros2_ws/src/hw1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'hw1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dylan',
+    maintainer_email='dhbrearley@crimson.ua.edu',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'diamond = hw1.diamond:main',
+            'square = hw1.squire:main',
+            'circle = hw1.circle:main',
+            'random = hw1.random:main',
+        ],
+    },
+)
